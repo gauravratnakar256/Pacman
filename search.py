3fe76b5f8b879a6f0c04fe9006da1ee31d544953
adroitx <abhi.sharma1177@gmail.com>
2022-10-22 21:41:03 -0700

    bfs modified

--- search.py
+++ search.py
@@ -100,47 +100,43 @@ def breadthFirstSearch(problem):
     visited[start_position] = True
 
     # path map.
-    map = {}
+    path = {}
     queue = util.Queue()
     for successor in problem.getSuccessors(problem.getStartState()):
         queue.push(successor)
-        map[successor] = start_position
-
-    # result list consists the directions.
-    res_list = []
-
+        path[successor] = start_position
+    
+    last_state = None
     # while queue is not empty, loop over it.
     while not queue.isEmpty():
         cur_state = queue.pop()
         cur_position = cur_state[0]
 
+        # if current position is goal state break out of while loop
+        if problem.isGoalState(cur_position):
+            last_state = cur_state
+            break
+
         # check whether current position is not visited and make it as true.
         if cur_position not in visited:
             visited[cur_position] = True
-
-            # if current position is goal state break out of while loop
-            if problem.isGoalState(cur_position):
-                break
 
             # if neighbours are not visited , push them into queue.
             for neighbour in problem.getSuccessors(cur_position):
                 if neighbour[0] not in visited:
                     queue.push(neighbour)
-                    map[neighbour] = cur_state
-
+                    path[neighbour] = cur_state
+                    
+    result = []
     # Create path from reverse traversing the map
-    direction = cur_state[1]
-    while cur_state != start_position:
-        state = map[cur_state]
-        if state != start_position:
-            res_list.append(state[1])
-        cur_state = state
-    res_list.insert(0, direction)
+    while last_state != start_position:
+        result.append(last_state[1])
+        last_state = path[last_state]
 
     #reverse the result as states were added  from goal to start position
-    res_list.reverse()
+    result.reverse()
     
-    return res_list
+    return result
 
 def uniformCostSearch(problem):
     """Search the node of least total cost first."""
