93b820bba797873efb61f94c19fa48953bb0b704
gauravratnakar256 <gauravgadewar35@hotmail.com>
2022-11-04 21:19:11 -0700

    find corner bfs

--- searchAgents.py
+++ searchAgents.py
@@ -269,7 +269,6 @@ def euclideanHeuristic(position, problem, info={}):
 class CornersProblem(search.SearchProblem):
     """
     This search problem finds paths through all four corners of a layout.
-
     You must select a suitable state space and successor function
     """
 
@@ -288,6 +287,8 @@ class CornersProblem(search.SearchProblem):
         # Please add any code here which you would like to use
         # in initializing the problem
         "*** YOUR CODE HERE ***"
+        #start state of pacman (position, corners)
+        self.startState = (self.startingPosition, ((-1, -1), (-1, -1), (-1, -1), (-1, -1)))
 
     def getStartState(self):
         """
@@ -295,19 +296,20 @@ class CornersProblem(search.SearchProblem):
         space)
         """
         "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        return self.startState
 
     def isGoalState(self, state):
         """
         Returns whether this search state is a goal state of the problem.
         """
         "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        if state[1] == self.corners:
+            return True
+        return False
 
     def getSuccessors(self, state):
         """
         Returns successor states, the actions they require, and a cost of 1.
-
          As noted in search.py:
             For a given state, this should return a list of triples, (successor,
             action, stepCost), where 'successor' is a successor to the current
@@ -325,6 +327,29 @@ class CornersProblem(search.SearchProblem):
             #   hitsWall = self.walls[nextx][nexty]
 
             "*** YOUR CODE HERE ***"
+            #get successors as (state, action, cost) and state will be (position, corners)
+            #visitedCorners = list(state[1])
+            visitedCorners = state[1]
+            x,y = state[0]
+            dx,dy = Actions.directionToVector(action)
+            nextx,nexty = int(x + dx),int(y + dy)
+            hitsWall = self.walls[nextx][nexty]
+
+            if hitsWall:
+                continue
+
+            if (nextx,nexty) in self.corners and (nextx,nexty) not in visitedCorners:
+                index=-1
+                for i,corner in enumerate(self.corners):
+                    if corner == (nextx,nexty):
+                        index = i
+                visitedCorners[index] = (nextx,nexty)
+                nextState =  ((nextx,nexty),tuple(visitedCorners))
+
+                successors.append((nextState,action,1))
+            else:
+                nextState = ((nextx,nexty),state[1])
+                successors.append((nextState,action, 1))
 
         self._expanded += 1 # DO NOT CHANGE
         return successors
@@ -346,12 +371,9 @@ class CornersProblem(search.SearchProblem):
 def cornersHeuristic(state, problem):
     """
     A heuristic for the CornersProblem that you defined.
-
       state:   The current search state
                (a data structure you chose in your search problem)
-
       problem: The CornersProblem instance for this layout.
-
     This function should always return a number that is a lower bound on the
     shortest path from the state to a goal of the problem; i.e.  it should be
     admissible (as well as consistent).
@@ -360,7 +382,29 @@ def cornersHeuristic(state, problem):
     walls = problem.walls # These are the walls of the maze, as a Grid (game.py)
 
     "*** YOUR CODE HERE ***"
-    return 0 # Default to trivial solution
+
+    res = 0
+    unvisited_corners = []
+
+    #append all unvisited corners to list from current state
+    for i,corner in enumerate(corners):
+        if state[1][i] == corner:
+            continue
+        unvisited_corners.append(corner)
+
+    cur_point = state[0]
+
+    #loop through all unvisited corners and find the cost from current point to min distance corner
+    #and that corner to other min distance corner untill all unvisited corners are visited
+    while len(unvisited_corners)!=0:
+        min_distance = sys.maxsize
+        min_corner = (-1, -1)
+        for corner in unvisited_corners:
+            min_distance, min_corner = min((util.manhattanDistance(cur_point, corner),corner), (min_distance, min_corner))
+        unvisited_corners.remove(min_corner)
+        cur_point = min_corner
+        res = res + min_distance
+    return res
 
 class AStarCornersAgent(SearchAgent):
     "A SearchAgent for FoodSearchProblem using A* and your foodHeuristic"
